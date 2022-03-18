8305be7ff925c9cecb2f5ee3dd5f8cf42e1b25b1
arturs <arturs.kirtovskis@gmail.com>
2022-03-18 18:47:52 +0200

    feat: add deposit section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,15 +11,45 @@ const Home: NextPage = () => {
     <>
       <Container css={{ mt: '3%', mr: '3%', ml: '3%' }}>
         <Header />
-        <Flex css={{ mt: '10%' }} justify={'center'} direction={'column'}>
+        <Flex css={{ mt: '7%' }} justify={'center'} direction={'column'}>
           <TabNav activeRoute={activeRoute} />
-          <Flex justify={'center'}>
+          <Flex align="center" justify="center" direction={'column'}>
             <Text css={{ color: 'white', mt: '$10' }}>Total money pooled</Text>
-            <Flex>
-              <Text css={{ color: 'white', mt: '$10' }}>
-                Total money pooled
+
+            <Text
+              size={'md'}
+              className="nes-text is-success"
+              css={{ color: 'white', mt: '$3', fontSize: '36px' }}
+            >
+              <i className="nes-icon star"></i> STX 80.259{' '}
+              <i className="nes-icon star"></i>
+            </Text>
+            <Text size={'xs'} css={{ color: 'white', mt: '$3' }}>
+              ($60,354)
+            </Text>
+            <Text css={{ color: 'white', mt: '$10' }}>
+              Next cycle in: 8 days 5 hours
+            </Text>
+
+            <progress
+              style={{ maxWidth: '50%' }}
+              className="nes-progress is-warning"
+              value="50"
+              max="100"
+            ></progress>
+
+            <label for="name_field" style={{ textAlign: 'center' }}>
+              <Text css={{ textAlign: 'center', color: 'white', mt: '$3' }}>
+                Deposit amount
               </Text>
-            </Flex>
+            </label>
+            <input
+              style={{ maxWidth: '25%' }}
+              type="text"
+              id="name_field"
+              className="nes-input is-dark"
+              placeholder="STX 0.00"
+            ></input>
           </Flex>
         </Flex>
       </Container>
