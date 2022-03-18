9dcd35840337e7dcaa5e8885ffb2c7a8f5734fbd
Léo Pradel <pradel.leo@gmail.com>
2022-03-18 14:31:34 +0100

    Update stacks-bingo_test.ts

--- stacks-bingo/tests/stacks-bingo_test.ts
+++ stacks-bingo/tests/stacks-bingo_test.ts
@@ -1,26 +1,37 @@
-
-import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v0.14.0/index.ts';
-import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
+import {
+  Clarinet,
+  Tx,
+  Chain,
+  Account,
+  types,
+} from "https://deno.land/x/clarinet@v0.14.0/index.ts";
+import { assertEquals } from "https://deno.land/std@0.90.0/testing/asserts.ts";
 
 Clarinet.test({
-    name: "Ensure that <...>",
-    async fn(chain: Chain, accounts: Map<string, Account>) {
-        let block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 2);
+  name: "Ensure that user can participate in the next cycle",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    let wallet_1 = accounts.get("wallet_1")!;
 
-        block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 3);
-    },
+    let block = chain.mineBlock([
+    //   Tx.contractCall(
+    //     "stacks-bingo",
+    //     // TODO public function to participate in the next cycle
+    //     "",
+    //     // TODO cycle id is required?
+    //     [types.uint(100)],
+    //     wallet_1.address
+    //   ),
+    ]);
+    assertEquals(block.receipts.length, 0);
+    assertEquals(block.height, 2);
+
+    block = chain.mineBlock([
+      /*
+       * Add transactions with:
+       * Tx.contractCall(...)
+       */
+    ]);
+    assertEquals(block.receipts.length, 0);
+    assertEquals(block.height, 3);
+  },
 });
