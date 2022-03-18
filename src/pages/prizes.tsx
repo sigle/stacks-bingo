23580ca10f5f63a68313d091016bcc0d33ec406f
arturs <arturs.kirtovskis@gmail.com>
2022-03-18 17:48:22 +0200

    feat: add tab navigator

--- src/pages/prizes.tsx
+++ src/pages/prizes.tsx
@@ -1,12 +1,19 @@
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
 import { Header } from '../modules/layout/Header';
-import { Container, Box } from '../ui';
+import { TabNav } from '../modules/layout/TabNav';
+import { Container, Box, Flex } from '../ui';
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  const activeRoute = router.pathname;
   return (
     <>
       <Container css={{ mt: '3%', mr: '3%', ml: '3%' }}>
         <Header />
+        <Flex css={{ mt: '10%' }} justify={'center'}>
+          <TabNav activeRoute={activeRoute} />
+        </Flex>
         <Box>Prizes</Box>
       </Container>
     </>
