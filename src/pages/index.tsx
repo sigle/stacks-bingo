57106482c78494270e5a45e21cac2e29efb1c9cc
arturs <arturs.kirtovskis@gmail.com>
2022-03-18 13:22:01 +0200

    fix: remove redundant import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next';
 import { styled } from '../stitches.config';
-import 'nes.css/css/nes.min.css';
 
 const Box = styled('div', { background: '#c7c7c7', color: '#f6f6f6' });
 
